4886f5044d27a78b2773fd7d45179dd229db885d
VandrewD <andreu.valls.dorca@gmail.com>
2016-01-14 19:17:20 +0100

    equipo jugador funcionando con entrenador

---
+++ src/main/java/com/example/ligabaloncesto/domain/Entrenador.java
@@ -0,0 +1,107 @@
+package com.example.ligabaloncesto.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.example.ligabaloncesto.domain.util.CustomLocalDateSerializer;
+import com.example.ligabaloncesto.domain.util.ISO8601LocalDateDeserializer;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+
+/**
+ * A Entrenador.
+ */
+@Entity
+@Table(name = "ENTRENADOR")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Entrenador implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    
+    @Column(name = "nombre")
+    private String nombre;
+    
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
+    @JsonSerialize(using = CustomLocalDateSerializer.class)
+    @JsonDeserialize(using = ISO8601LocalDateDeserializer.class)
+    @Column(name = "fecha_nacimiento")
+    private LocalDate fechaNacimiento;
+
+    @OneToOne(mappedBy = "entrenador")
+    @JsonIgnore
+    private Equipo equipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Entrenador entrenador = (Entrenador) o;
+
+        if ( ! Objects.equals(id, entrenador.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Entrenador{" +
+                "id=" + id +
+                ", nombre='" + nombre + "'" +
+                ", fechaNacimiento='" + fechaNacimiento + "'" +
+                '}';
+    }
+}
